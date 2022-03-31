41271afbaa8c46aeae01b2233dcd12596f4d127a
markallanjohnson <markallanjohnson1@gmail.com>
2022-03-31 07:44:39 -0600

    hide api...

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ parameters = {
 }
 headers = {
   'Accepts': 'application/json',
-  #'X-CMC_PRO_API_KEY': key,
+  'X-CMC_PRO_API_KEY': 'KEY'
 }
 
 session = Session()
@@ -29,11 +29,14 @@ total_market_cap = 0
 
 for coin in data:
     name = coin['name']
-    cap = int(coin['quote']['USD']['market_cap'])
+    cap = coin['quote']['USD']['market_cap']
     crypto_info[name] = {'market_cap': cap}
-    total_market_cap += cap
+    total_market_cap += int(cap)
 
 # cache data
 # when ran again, compare values, notify if new entrant. detect if
 # coin climbs a tier. don't get confused if a coin that was previously
 # in the top 25 makes it back again.
+for item in crypto_info.items():
+    cap = item[1]['market_cap']
+    print(item[0] + ", " + str(cap) + ", " + str(cap / total_market_cap))
